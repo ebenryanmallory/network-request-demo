9a77e85d08b4bb9ef111493aaf9ac634bf0972ba
Eben Mallory <ebenryanmallory@gmail.com>
2020-01-17 21:21:42 -0700

    initial code for all 4 styles

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 // Apparently body-parser is included in Express 4+, opportunity to refactor and uninstall module
 const bodyParser = require('body-parser')
+const formidable = require('formidable')
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -14,8 +15,18 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
   .get('/', (req, res) => res.render('pages/index'))
   .use('/api/card', (req, res) => {
     res.json(HTMLObject);
+  })
+  .post('/api/form', (req, res) => {
+    new formidable.IncomingForm().parse(req, (err, fields) => {
+      if (err) {
+        console.error('Error', err)
+        throw err
+      }
+      if (req) { res.json([HTMLObject, fields]) };
+    })
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
